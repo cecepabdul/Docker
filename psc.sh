58dd1b49460a5b01ace5bbfdc1c69e8640038ec4
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-06-01 06:32:21 +0700

    Create psc.sh

---
+++ psc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Step 1: Unduh dan ekstrak file rhminer
+wget https://github.com/polyminer1/rhminer/releases/download/2.3b/rhminer.2.3.Linux.CPU_OLDGEN.zip
+unzip rhminer.2.3.Linux.CPU_OLDGEN.zip
+
+# Step 2: Buat file konfigurasi systemd
+sudo tee /etc/systemd/system/rhminer.service <<EOF
+[Unit]
+Description=Rhminer Service
+After=network.target
+
+[Service]
+ExecStart=/bin/bash -c "cd /root && ./rhminer -v 2 -r 20 -s stratum+tcp://pascal.tucanpool.com:3334 -su 1140649-43.3A5ADE982ACB80C4.cl/ceceptkj67@gmail.com -cpu"
+WorkingDirectory=/root
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Step 3: Setel izin pada file konfigurasi
+sudo chmod 644 /etc/systemd/system/rhminer.service
+
+# Step 4: Muat ulang konfigurasi systemd
+sudo systemctl daemon-reload
+
+# Step 5: Mulai layanan rhminer
+sudo systemctl start rhminer
+
+# Step 6: Aktifkan layanan rhminer saat sistem boot
+sudo systemctl enable rhminer
