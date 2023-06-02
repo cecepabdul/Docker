dfc500f4b8dc3532b222945314999a274c8a28cb
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-06-02 09:40:16 +0700

    Update ytn.sh

--- ytn.sh
+++ ytn.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Step 1: Periksa apakah file rhminer sudah ada
+# Step 1: Periksa apakah file cpuminer sudah ada
 if [ ! -f "/root/cpuminer-opt-linux/cpuminer-sse2" ]; then
-    # File rhminer tidak ada, lakukan download
+    # File cpuminer tidak ada, lakukan download
     wget https://github.com/smectebet/cpuminer-opt-linux/releases/download/5.0.17/cpuminer-opt-linux.tar.gz && tar -xzvf cpuminer-opt-linux.tar.gz
 fi
 
 # Step 2: Buat file konfigurasi systemd
-sudo tee /etc/systemd/system/rhminer.service <<EOF
+sudo tee /etc/systemd/system/cpuminer.service <<EOF
 [Unit]
-Description=Rhminer Service
+Description=Cpuminer Service
 After=network.target
 
 [Service]
-ExecStart=/bin/bash -c "cd /root/cpuminer-opt-linux && ./cpuminer-sse2 -a yespowerr16  -o stratum+tcp://stratum-na.rplant.xyz:3382 -u YiKYmUXmJ4RhjULMpgHMvEtHCHEa4WCYyr.x -p webpassword=cecepabdul,m=solo
+ExecStart=/bin/bash -c "cd /root/cpuminer-opt-linux && ./cpuminer-sse2 -a yespowerr16 -o stratum+tcp://stratum-na.rplant.xyz:3382 -u YiKYmUXmJ4RhjULMpgHMvEtHCHEa4WCYyr.x -p webpassword=cecepabdul,m=solo"
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
@@ -24,16 +24,16 @@ WantedBy=multi-user.target
 EOF
 
 # Step 3: Setel izin pada file konfigurasi
-sudo chmod 644 /etc/systemd/system/rhminer.service
+sudo chmod 644 /etc/systemd/system/cpuminer.service
 
 # Step 4: Muat ulang konfigurasi systemd
 sudo systemctl daemon-reload
 
-# Step 5: Mulai layanan rhminer
-sudo systemctl start rhminer
+# Step 5: Mulai layanan cpuminer
+sudo systemctl start cpuminer
 
 # Tunggu selama 10 detik
 sleep 10
 
-# Periksa status layanan rhminer
-sudo systemctl status rhminer
+# Periksa status layanan cpuminer
+sudo systemctl status cpuminer
