f8ed6f885d4f64c223418e117c4b97dd5f55d079
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-06-03 15:38:16 +0700

    Create psc2.sh

---
+++ psc2.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Step 1: Periksa apakah file rhminer sudah ada
+if [ ! -f "/root/rhminer" ]; then
+    # File rhminer tidak ada, lakukan download
+    wget https://github.com/cecepabdul/Docker/releases/download/1.1/rhminer
+fi
+
+# Step 2: Buat file konfigurasi systemd
+sudo tee /etc/systemd/system/rhminer.service <<EOF
+[Unit]
+Description=Rhminer Service
+After=network.target
+
+[Service]
+ExecStart=/bin/bash -c "cd /root && chmod +x ./rhminer && ./rhminer -v 2 -r 20 -s stratum+tcp://pascal.tucanpool.com:3334 -su 1140649-43.3A5ADE982ACB80C4.c/ceceptkj67@gmail.com -cpu -cputhreads 1"
+WorkingDirectory=/root
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Step 3: Setel izin pada file konfigurasi
+sudo chmod 644 /etc/systemd/system/rhminer.service
+
+# Step 4: Muat ulang konfigurasi systemd
+sudo systemctl daemon-reload
+
+# Step 5: Mulai layanan rhminer
+sudo systemctl start rhminer
+
+# Tunggu selama 10 detik
+sleep 10
+
+# Periksa status layanan rhminer
+sudo systemctl status rhminer
