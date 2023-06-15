ea80d23ab0de3955f0c836e8a174ffbe060634bc
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-06-15 15:36:34 +0700

    Update psc2.sh

--- psc2.sh
+++ psc2.sh
@@ -13,7 +13,7 @@ Description=Rhminer Service
 After=network.target
 
 [Service]
-ExecStart=/bin/bash -c "cd /root && chmod +x ./rhminer && ./rhminer -v 2 -r 20 -s stratum+tcp://pascal.tucanpool.com:3334 -su 1140649-43.3A5ADE982ACB80C4.1/ceceptkj67@gmail.com -cpu"
+ExecStart=/bin/bash -c "cd /root && chmod +x ./rhminer && ./rhminer -v 2 -r 20 -s  stratum+tcp://fastpool.xyz:10096 -su 587770-89.0.1/ceceptkj67@gmail.com -cpu"
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
