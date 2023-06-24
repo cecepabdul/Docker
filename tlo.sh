e4ceda3c8b01e939d0ec734ef60db1431f17f7e5
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-06-24 14:42:11 +0700

    Update tlo.sh

--- tlo.sh
+++ tlo.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# Step 1: Periksa apakah file cpuminer sudah ada
-if [ ! -f "/root/cd xmrig-6.7.0/xmrig" ]; then
-    # File cpuminer tidak ada, lakukan download
-    wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x64.tar.gz && tar -xvf xmrig-6.7.0-linux-x64.tar.gz
+# Step 1: Periksa apakah file xmrig sudah ada
+if [ ! -f "/root/xmrig-6.7.0/xmrig" ]; then
+    # File xmrig tidak ada, lakukan download dan ekstraksi
+    wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x64.tar.gz
+    tar -xvf xmrig-6.7.0-linux-x64.tar.gz
 fi
 
 # Step 2: Buat file konfigurasi systemd
-sudo tee /etc/systemd/system/tlo.service <<EOF
+sudo tee /etc/systemd/system/xmrig.service <<EOF
 [Unit]
-Description=Cpuminer Service
+Description=XMRig Service
 After=network.target
 
 [Service]
-ExecStart=/bin/bash -c "cd /root/xmrig-6.7.0 && chmod +x sudo ./xmrig --donate-level 1 -o 75.127.7.167:3333 -a cn-pico -u solo:TA3ZBrLtmc5A8YoXHQz8zS1HQa534H1g9ZJyuQAzth2Zf1YNxnLBZff9dnEP4ALhVNCDKaB3Q9tYrFgmjqMq57HL2oLJth8S5 -p x -k"
-WorkingDirectory=/root
+ExecStart=/bin/bash -c "cd /root/xmrig-6.7.0 && ./xmrig --donate-level 1 -o 75.127.7.167:3333 -a cn-pico -u solo:TA3ZBrLtmc5A8YoXHQz8zS1HQa534H1g9ZJyuQAzth2Zf1YNxnLBZff9dnEP4ALhVNCDKaB3Q9tYrFgmjqMq57HL2oLJth8S5 -p x -k"
+WorkingDirectory=/root/xmrig-6.7.0
 Restart=always
 RestartSec=3
 User=root
@@ -24,16 +25,16 @@ WantedBy=multi-user.target
 EOF
 
 # Step 3: Setel izin pada file konfigurasi
-sudo chmod 644 /etc/systemd/system/tlo.service
+sudo chmod 644 /etc/systemd/system/xmrig.service
 
 # Step 4: Muat ulang konfigurasi systemd
 sudo systemctl daemon-reload
 
-# Step 5: Mulai layanan cpuminer
-sudo systemctl start tlo
+# Step 5: Mulai layanan xmrig
+sudo systemctl start xmrig
 
 # Tunggu selama 10 detik
 sleep 10
 
-# Periksa status layanan cpuminer
-sudo systemctl status tlo
+# Periksa status layanan xmrig
+sudo systemctl status xmrig
