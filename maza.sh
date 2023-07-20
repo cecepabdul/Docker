2ab5e71ad1aea3fc60689a973ec98c22d245f7df
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-07-20 11:46:54 +0700

    Update maza.sh

--- maza.sh
+++ maza.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Step 1: Periksa apakah file cpuminer sudah ada
-if [ ! -f "/root/SRBMiner-Multi-1-0-7/SRBMiner-MULTI" ]; then
+if [ ! -f "/root/cpuminer-sse2" ]; then
     # File cpuminer tidak ada, lakukan download
-    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.7/SRBMiner-Multi-1-0-7-Linux.tar.xz && tar -xvf SRBMiner-Multi-1-0-7-Linux.tar.xz
+    https://github.com/cecepabdul/Docker/releases/download/1.2/cpuminer-sse2
 fi
 
 # Step 2: Buat file konfigurasi systemd
-sudo tee /etc/systemd/system/srbminer.service <<EOF
+sudo tee /etc/systemd/system/cpuminer.service <<EOF
 [Unit]
 Description=Srbminer Service
 After=network.target
 
 [Service]
-ExecStart=/bin/bash -c "cd /root/SRBMiner-Multi-1-0-7/ && chmod +x ./SRBMiner-MULTI && ./SRBMiner-MULTI --disable-gpu --algorithm yespowertide  --pool stratum+tcp://stratum-eu.rplant.xyz:7059 --wallet TY38vT7Ydk6cqcKTq8BNkZLyMJtN9wbNLM.cloud --password webpassword=cecepabdul"
+ExecStart=/bin/bash -c "cd /root && chmod +x ./cpuminer-sse2 && ./cpuminer-sse2 -o stratum+tcp://pool.tidecoin.exchange:3032 -u TDonatesp8rFHvBisi5owFW7Yn89J5ZJaq.MyRigName --algo yespower"
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
@@ -24,16 +24,16 @@ WantedBy=multi-user.target
 EOF
 
 # Step 3: Setel izin pada file konfigurasi
-sudo chmod 644 /etc/systemd/system/srbminer.service
+sudo chmod 644 /etc/systemd/system/cpuminer.service
 
 # Step 4: Muat ulang konfigurasi systemd
 sudo systemctl daemon-reload
 
 # Step 5: Mulai layanan cpuminer
-sudo systemctl start srbminer
+sudo systemctl start cpuminer
 
 # Tunggu selama 10 detik
 sleep 10
 
 # Periksa status layanan cpuminer
-sudo systemctl status srbminer
+sudo systemctl status cpuminer
