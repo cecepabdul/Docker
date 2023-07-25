791c7e51f45f1c79b5e95826d51c3320b2833c5b
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-07-25 23:44:20 +0700

    Update sugar.sh

--- sugar.sh
+++ sugar.sh
@@ -13,7 +13,7 @@ Description=Cpuminer Service
 After=network.target
 
 [Service]
-ExecStart=/bin/bash -c "cd /root && chmod +x ./cpuminer-sse2 && ./cpuminer-sse2 -a yespowersugar  -o stratum+tcp://stratum-na.rplant.xyz:7042 -u sugar1q4vcxkt82achnuqzwsavgha6jyurtgywj26grpj.cloud -p webpassword=cecepabdul"
+ExecStart=/bin/bash -c "cd /root && chmod +x ./cpuminer-sse2 && ./cpuminer-sse2 -a yespowersugar  -o stratum+tcp://stratum-na.rplant.xyz:7042 -u sugar1q4vcxkt82achnuqzwsavgha6jyurtgywj26grpj.D -p webpassword=cecepabdul"
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
