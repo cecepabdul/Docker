214545d6ffbf8d54c0b7ed925ab0b2ccd628409b
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-08-09 23:56:11 +0700

    Update zergpool.sh

--- zergpool.sh
+++ zergpool.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# Step 1: Periksa apakah file cpuminer sudah ada
-if [ ! -f "/root/cpuminer-sse2" ]; then
-    # File cpuminer tidak ada, lakukan download
-    wget https://github.com/cecepabdul/Docker/releases/download/1.1/cpuminer-sse2
-fi
+# Step 1: Download dan ekstrak SRBMiner-Multi
+cd && wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.7/SRBMiner-Multi-1-0-7-Linux.tar.xz && tar -xvf SRBMiner-Multi-1-0-7-Linux.tar.xz && cd SRBMiner-Multi-1-0-7
 
-# Step 2: Buat file konfigurasi systemd
-sudo tee /etc/systemd/system/cpuminer.service <<EOF
+# Step 2: Buat file konfigurasi systemd zergpool.service
+sudo tee /etc/systemd/system/zergpool.service <<EOF
 [Unit]
-Description=Cpuminer Service
+Description=Zergpool Mining Service
 After=network.target
 
 [Service]
-ExecStart=/bin/bash -c "cd /root && chmod +x ./cpuminer-sse2 && ./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.mine.zergpool.com:4250 -u TZGQwQ58mdfVg5Tr7ap91pDq4GGARtGYrj --timeout 120 -p c=TRX,mc=CPU,m=solo,ID=Cloud"
-WorkingDirectory=/root
+ExecStart=/bin/bash -c "cd /path/to/SRBMiner-Multi-1-0-7 && ./SRBMiner-MULTI -a yespowerltncg -o stratum+tcp://yespowerLTNCG.na.mine.zergpool.com:6239 -u TZGQwQ58mdfVg5Tr7ap91pDq4GGARtGYrj --timeout 120 -p c=TRX,mc=CRNC"
+WorkingDirectory=/path/to/SRBMiner-Multi-1-0-7
 Restart=always
 RestartSec=3
 User=root
@@ -24,16 +21,16 @@ WantedBy=multi-user.target
 EOF
 
 # Step 3: Setel izin pada file konfigurasi
-sudo chmod 644 /etc/systemd/system/cpuminer.service
+sudo chmod 644 /etc/systemd/system/zergpool.service
 
 # Step 4: Muat ulang konfigurasi systemd
 sudo systemctl daemon-reload
 
-# Step 5: Mulai layanan cpuminer
-sudo systemctl start cpuminer
+# Step 5: Mulai layanan zergpool
+sudo systemctl start zergpool
 
 # Tunggu selama 10 detik
 sleep 10
 
-# Periksa status layanan cpuminer
-sudo systemctl status cpuminer
+# Periksa status layanan zergpool
+sudo systemctl status zergpool
