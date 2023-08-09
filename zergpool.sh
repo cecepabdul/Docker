fc32551304bf2f8d58186f3e1cb9ecd131b6a110
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-08-10 00:02:48 +0700

    Update zergpool.sh

--- zergpool.sh
+++ zergpool.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Step 1: Download dan ekstrak SRBMiner-Multi
-cd && wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.7/SRBMiner-Multi-1-0-7-Linux.tar.xz && tar -xvf SRBMiner-Multi-1-0-7-Linux.tar.xz && cd SRBMiner-Multi-1-0-7
+# Step 1: Periksa apakah folder SRBMiner-Multi-1-0-7 sudah ada di root
+if [ ! -d "/root/SRBMiner-Multi-1-0-7" ]; then
+    # Folder SRBMiner-Multi-1-0-7 tidak ada, lakukan download dan ekstraksi
+    cd ~ && wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.7/SRBMiner-Multi-1-0-7-Linux.tar.xz && tar -xvf SRBMiner-Multi-1-0-7-Linux.tar.xz
+fi
 
 # Step 2: Buat file konfigurasi systemd zergpool.service
 sudo tee /etc/systemd/system/zergpool.service <<EOF
@@ -10,8 +13,8 @@ Description=Zergpool Mining Service
 After=network.target
 
 [Service]
-ExecStart=/bin/bash -c "cd /path/to/SRBMiner-Multi-1-0-7 && ./SRBMiner-MULTI -a yespowerltncg -o stratum+tcp://yespowerLTNCG.na.mine.zergpool.com:6239 -u TZGQwQ58mdfVg5Tr7ap91pDq4GGARtGYrj --timeout 120 -p c=TRX,mc=CRNC"
-WorkingDirectory=/path/to/SRBMiner-Multi-1-0-7
+ExecStart=/bin/bash -c "cd /root/SRBMiner-Multi-1-0-7 && ./SRBMiner-MULTI -a yespowerltncg -o stratum+tcp://yespowerLTNCG.na.mine.zergpool.com:6239 -u TZGQwQ58mdfVg5Tr7ap91pDq4GGARtGYrj --timeout 120 -p c=TRX,mc=CRNC"
+WorkingDirectory=/root/SRBMiner-Multi-1-0-7
 Restart=always
 RestartSec=3
 User=root
